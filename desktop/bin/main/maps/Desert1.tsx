3e3d243627c74d0d8c93be2b9203ec10f94536d6
Gaiutuz <86575936+Gaiutuz@users.noreply.github.com>
2024-07-23 23:53:01 -0300

    Add files via upload

---
+++ desktop/bin/main/maps/Desert1.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="3DS - New Super Mario Bros 2 - Desert" tilewidth="20" tileheight="20" tilecount="2205" columns="21">
+ <image source="3DS - New Super Mario Bros 2 - Desert.png" width="420" height="2100"/>
+ <tile id="1">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
