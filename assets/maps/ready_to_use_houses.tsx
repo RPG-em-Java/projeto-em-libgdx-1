3728107035530cbf785bbb84e439820f7fd32d39
Gaiutuz <86575936+Gaiutuz@users.noreply.github.com>
2024-07-23 23:49:07 -0300

    Add files via upload

---
+++ assets/maps/ready_to_use_houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="ready_to_use_houses" tilewidth="20" tileheight="20" tilecount="1456" columns="91">
+ <image source="ready_to_use_houses.png" width="1824" height="320"/>
+</tileset>
